4667ec10a15e30bf5c86592cf26c141bc0ff5f38
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-20 05:01:24 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'sweet', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'sweet', true);
