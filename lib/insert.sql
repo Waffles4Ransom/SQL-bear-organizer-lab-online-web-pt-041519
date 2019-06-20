5003eac35dd41b56527859377e5a56e7c09c3b3a
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-20 05:04:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'sweet', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'male', 'black', 'energetic', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'female', 'brown', 'sweet', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergant Brown', 6, 'female', 'brown', '', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'female', 'brown', 'affectionate', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergant Brown', 6, 'female', 'brown', 'strict', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'female', 'black', 'serious', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'sweet', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'sweet', true);
